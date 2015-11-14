f8ee5b1077e6ab8e9731c16404a39e514a2b7b7f
Sergio Rodrigo <srodrigo@MacBook-Pro-de-Sergio.local>
2015-11-14 21:59:42 +0100

    Changed invoker to user ExecutorService instead of a ThreadPoolExecutor.

--- presentation/src/main/kotlin/me/srodrigo/kotlinwars/CommandInvokerImp.kt
+++ presentation/src/main/kotlin/me/srodrigo/kotlinwars/CommandInvokerImp.kt
@@ -1,21 +1,14 @@
 package me.srodrigo.kotlinwars
 
 import me.srodrigo.kotlinwars.infrastructure.*
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.ThreadPoolExecutor
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.*
 
 class CommandInvokerImp(private val postExecutionThread: ExecutionThread) : CommandInvoker {
 
-	private val corePoolSize = 3
-	private val maxPoolSize = 4
-	private val keepAliveTime = 120L
-	private val timeUnit = TimeUnit.SECONDS
-	private val workQueue = LinkedBlockingQueue<Runnable>()
-	private val threadPoolExecutor =
-			ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, timeUnit, workQueue)
+	private val executor: ExecutorService =
+			Executors.newScheduledThreadPool(2 * Runtime.getRuntime().availableProcessors())
 
 	override fun <T : CommandResponse<out Any>> execute(execution: CommandExecution<T>) {
-		threadPoolExecutor.submit(CommandInvokerTask(execution, postExecutionThread))
+		executor.submit(CommandInvokerTask(execution, postExecutionThread))
 	}
 }
